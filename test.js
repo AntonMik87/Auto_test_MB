a2ba2ceb1a4b9152afefc93be05e69ca69d8753b
AntonMik87 <antonrabota09@mail.ru>
2023-12-17 20:04:33 +0700

    Select_option_2_win

--- test.js
+++ test.js
@@ -1,31 +1,31 @@
-const webDriver = require('selenium-webdriver');
-driver = new webDriver.Builder().forBrowser('chrome').build();
-const By = webDriver.By
+const webdriver = require('selenium-webdriver');
+driver = new webdriver.Builder().forBrowser('chrome').build();
+const By = webdriver.By
+
+const assert = require('assert/strict')
+const {Select} = require('selenium-webdriver')
 
 async function testRun(){
     await driver.manage().setTimeouts({implicit: 2000})
     await driver.get('https://testdrive.andersenlab.com');
 
-    
+   // driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     await driver.findElement(By.xpath('//*[@id="slider"]/span[1]')).click();
   
     await driver.findElement(By.xpath('//*[@id="go_to_step_2"]')).click();
-    
-    await driver.findElement(By.xpath('//*[@id="engine"]')).click();
-    await driver.findElement(By.css('#engine > option:nth-child(2)'))
-    .click();
-
-   //await driver.findElement(By.xpath('//*[@id="engine"]/option[2]')).click();
-    
+   
+    // await driver.findElement(By.xpath('//*[@id="engine"]')).click();
+    const loc = await driver.findElement(By.xpath('//select[@id="engine"]')).click();
+    await driver.findElement(By.xpath('//*[@id="engine"]/option[2]')).click();
 }
-
-
-
 testRun();
 
+ 
 
+//*[@id="engine"]
+//*[@id="engine"]/option[2]
+// #engine > option:nth-child(2) 
 
+//*[@id="engine"]/option[2]
 
-
-
-
+//*[@id="engine"]
